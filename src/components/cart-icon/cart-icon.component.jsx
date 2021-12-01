b63d6060bbb6e23924664451a151623e5c9ce554
amarkevych <aw.markevich@gmail.com>
2021-12-01 18:16:06 +0200

    Add cart icon count

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,11 +1,12 @@
 import React, {useCallback} from "react";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {toggleCartHidden} from "../../redux/cart/cart.actions";
 import {ReactComponent as ShoppingIcon} from "../../assets/shopping-bag.svg";
 import "./cart-icon.styles.scss";
 
 const CartIcon = () => {
     const dispatch = useDispatch()
+    const itemsCount = useSelector(state => state.cart.cartItems.reduce((acc, value) => acc + value.quantity, 0))
 
     const onToggleCartHidden = useCallback(() => {
         dispatch(toggleCartHidden())
@@ -14,7 +15,7 @@ const CartIcon = () => {
     return (
         <div className="cart-icon" onClick={onToggleCartHidden}>
             <ShoppingIcon className="shopping-icon"/>
-            <span className="item-count">0</span>
+            <span className="item-count">{itemsCount}</span>
         </div>
     )
 
